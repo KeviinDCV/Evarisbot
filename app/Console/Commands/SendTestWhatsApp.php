a638a8d04d345140f2f11e797cb0fe507ee932d8
KeviinDCV <110347632+KeviinDCV@users.noreply.github.com>
2025-10-21 14:22:10 -0500

    7

---
+++ app/Console/Commands/SendTestWhatsApp.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SendTestWhatsApp extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-test-whats-app';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
